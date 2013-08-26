a221667aaf993141ac5494f1da71e14753e685a0
dmcb <d.mcburney@gmail.com>
2013-08-26 17:08:00 -0600

    Fixed broken views definition.
    Views as defined in Kanso were nested under 'views' which Couch doesn't
    understand.

--- ucpl-app/lib/views.js
+++ ucpl-app/lib/views.js
@@ -1,9 +1,7 @@
-exports.views = {
-	depts: {
-		map: function(doc) {
-			if (doc.faculty_descr) {
-		 		emit(doc.faculty_descr, {"departments":doc.departments, "faculty_code":doc.faculty_code});
-			}
+exports.depts = {
+	map: function(doc) {
+		if (doc.faculty_descr) {
+	 		emit(doc.faculty_descr, {"departments":doc.departments, "faculty_code":doc.faculty_code});
 		}
 	}
 }
